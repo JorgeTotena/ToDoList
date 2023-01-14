795bc78f6df949d02838d50fe4a8045ad2f9faf8
jorge-totena-windows <jorge.totena@gmail.com>
2023-01-14 11:18:18 -0500

    Creating the add functionality for the program

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 print('''Welcome to your to-do list app
 These are your current to-dos''')
-def todolist(position):
-    todolist = [{"todo description": "Learn Python", "done": False}, 
+def todolist():
+    todolist = [{"todo description": "Learn Python", "done": False},  
                 {"todo description": "Walk the dog", "done": False}]
-    print(todolist[position])
+    print(todolist)
     return todolist
 
-todos = todolist(int(input("Input your task number -> ")))
+def add_todos(todos):
+    confirmation = input("Do you want to add a new todo? y/n ")
+    
+    if confirmation == "y":
+        addition = todos.append({"todo description": input("What's your new todo"), "done": False})
+        print(todos)
+        return addition
+    else:
+        print("thank you for using this program")
+
+todos = todolist()
+addition = add_todos(todos)
