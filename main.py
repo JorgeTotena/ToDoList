37a6a41495a3d68657f19e5dccb2a1fc8117c765
ravenclawgt <jorge.totena@gmail.com>
2023-01-16 11:28:11 -0500

    Adding the list comprehension an option menu

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ todolist = [{"todo description": "Learn Python", "due date": "01/14/2023"},
             {"todo description": "Walk the dog", "due date": "01/14/2023"}
             ]
 print(todolist)
-  
 
 def add_todos(todolist):
     confirmation = input("Do you want to add a new todo? y/n ")
@@ -19,11 +18,32 @@ def delete_todos(todolist):
     if confirmation == "y":
         pass
     
-addition = add_todos(todolist)
+# addition = add_todos(todolist)
+
+# escription = input("Input the description you want to delete -> ")
+#todolist = [item for item in todolist if item["todo description"] != description]
 
 print(todolist)
+change = input("Would you like to change something in your list y/n -> ")
+if change == "y":
+    option = input(''' Select your option (in numbers)
+    1. Add todos
+    2. Delete todos
+    3. Update todos
+    4. Consult todos                   
+''')
+else:
+    print("Choose a correct option")    
+
+
+'''index = list(todolist[0].keys()).pop("todo description")
+print(index) '''
+
 # GETS THE index within the dictionary stored in the list
 ''' index = list(todolist[0].keys()).index("todo description")
 print(index) '''
-''' index = list(todolist[0].keys()).pop("todo description")
-print(index) '''
+
+#REMOVING A VALUE FROM THE TODOLIST WITH A STANDARD LIST
+"""for item in todolist:
+    if item["todo description"] == "Learn Python":
+        todolist.remove(item) """
