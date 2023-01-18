09849fc910791223bb60c8992cef779fb15b334d
jorge-totena-windows <jorge.totena@gmail.com>
2023-01-17 23:28:16 -0500

    Option menu and functions within the menu

--- main.py
+++ main.py
@@ -5,25 +5,16 @@ todolist = [{"todo description": "Learn Python", "due date": "01/14/2023"},
             ]
 print(todolist)
 
-def add_todos(todolist):
-    confirmation = input("Do you want to add a new todo? y/n ")
-    if confirmation == "y":
-        addition = todolist.append({"todo description": input("What's your new todo "), "due data": input('When is it due -> mm/dd/aaaa ')})
-        return addition
-    else:
-        print("thank you for using this program")
-
-def delete_todos(todolist):
-    confirmation = input("Do you want to delete a todo y/n ")
-    if confirmation == "y":
-        pass
+def add_todos(todolist):    
+    todolist = todolist.append({"todo description": input("What's your new todo "), "due data": input('When is it due -> mm/dd/aaaa ')})
+    return todolist
     
-# addition = add_todos(todolist)
+def delete_todos(todolist):    
+    description = input("Input the description you want to delete -> ")
+    todolist = [item for item in todolist if item["todo description"] != description]
+    return todolist
+    
 
-# escription = input("Input the description you want to delete -> ")
-#todolist = [item for item in todolist if item["todo description"] != description]
-
-print(todolist)
 change = input("Would you like to change something in your list y/n -> ")
 if change == "y":
     option = input(''' Select your option (in numbers)
@@ -32,8 +23,20 @@ if change == "y":
     3. Update todos
     4. Consult todos                   
 ''')
+    if option == "1":
+        addition = add_todos(todolist)
+    elif option == "2":
+        todolist = delete_todos(todolist)
+    else:
+        print("you did not input a correct option")
+        
 else:
     print("Choose a correct option")    
+
+print(todolist)
+
+
+
 
 
 '''index = list(todolist[0].keys()).pop("todo description")
